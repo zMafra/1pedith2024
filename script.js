72d9c46a6e4281c5bf2fe7030022a283dc0731c7
zMafra <mota.neto@escola.pr.gov.br>
2024-10-28 09:43:09 -0300

    aula 1

---
+++ script.js
@@ -0,0 +1,23 @@
+// Matrix, LIVRE
+// A Teia, 16
+// Abigail, 16
+// Feios, 14
+// A liga, LIVRE
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(200);
+}
+function setup() {
+  background(200);
+  let redomendacao = "Matrix";
+  text (recomendacao, width / 2, height / 2);
+}
+function geraREcomendacao(idade) {}
+  if(idade >= 10) {
+  return "Matriz";
+} else {
+  return "a Liga";
+}
